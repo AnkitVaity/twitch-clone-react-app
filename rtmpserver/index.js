895da8bc765ec7688115fa635aa00cf00bf9c740
Ankit Vaity <30876031+THEANKITVAITY@users.noreply.github.com>
2020-06-28 15:54:39 +0530

    initial upload

---
+++ rtmpserver/index.js
@@ -0,0 +1,22 @@
+// StreamShow4-----------------------------------------------------------------
+// StreamShow5 is in rtmpserver/package.json (start it using npm start)
+// StreamStep6 is to install OBS on your Desktop machine
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
+// ----------------------------------------------------------------------------
